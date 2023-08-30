91a7741d13d3d92e1807509dbe02af2022628325
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2023-08-30 16:35:42 +0200

    Update send_packet.m

--- Research/send_packet.m
+++ Research/send_packet.m
@@ -1,10 +1,10 @@
 function [keepalive, error]=send_packet(packet_TX)
 global arduinoObj
 packet_size=length(packet_TX);
-out=[];
 for i=1:1:packet_size
     fwrite(arduinoObj,packet_TX(i),"uint8");
     pause(0.0001);
+    out=[];
     out=fread(arduinoObj,1,"uint8");
     flush(arduinoObj);
     if i==packet_size-1;
